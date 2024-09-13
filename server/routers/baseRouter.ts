16f1143036a0e507960b179503d59534b2615b64
Nick Forleo <nickm140mc@gmail.com>
2024-09-12 20:59:33 -0400

    set up more backend struture; start auth endpoint

---
+++ server/routers/baseRouter.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import authRouter from "./authRouter";
+
+const router = express.Router();
+
+router.use(`/auth`, authRouter);
---
+++ server/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const port = 5000;
+
+dotenv.config();
+
+var spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+var app = express();
+
+app.get("/auth/login", (req, res) => {});
+
+app.get("/auth/callback", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+  console.log(`spotify stuff`, { spotify_client_id, spotify_client_secret });
+});
