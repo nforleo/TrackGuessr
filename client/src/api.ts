4439cf680b28fbcd2bfa2a3a70564575de50d6db
Nick Forleo <nickm140mc@gmail.com>
2024-09-17 15:10:09 -0400

    build out main home screen with button components

--- client/src/api.ts
+++ client/src/api.ts
@@ -2,12 +2,18 @@ import axios from 'axios';
 
 // const baseURL = 'http://localhost:5000/api'
 
-export async function getAuthToken(): Promise<unknown> {
+export async function getAuthToken(): Promise<{ access_token: string}> {
     const { data } = await axios({
         method: 'get',
-        // baseURL,
         url: `/auth/token`
     });
 
     return data;
+}
+
+export async function logout(): Promise<void> {
+    await axios({
+        method: 'post',
+        url: '/auth/logout'
+    });
 }
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,40 @@
+// import React, { useState, useEffect } from 'react';
+
+import { useEffect } from "react";
+import { Button, Stack } from "react-bootstrap";
+import { logout } from "../api";
+
+interface HomeInterface {
+    token: string
+    setToken: (str: string) => void;
+}
+
+function Home({
+  token,
+  setToken
+}: HomeInterface) {
+    const _logout = () => {
+      logout();
+      setToken('');
+    }
+   return (
+            <Stack gap={3} direction="vertical">
+                <Button>
+                  Daily
+                </Button>
+                <Button>
+                  Custom
+                </Button>
+                <Button>
+                  Stats
+                </Button>
+                <Button onClick={() => {
+                  _logout()
+                }}>
+                  Log Out
+                </Button>
+              </Stack>
+    );
+}
+
+export default Home
