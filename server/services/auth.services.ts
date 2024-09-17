8444adcb46d185066e72ab70f48649044be103c4
Nick Forleo <nickm140mc@gmail.com>
2024-09-17 15:04:07 -0400

    last of logout; allow switch user when authenticating

--- server/services/auth.services.ts
+++ server/services/auth.services.ts
@@ -39,6 +39,7 @@ export const login = (): string => {
     scope: scope,
     redirect_uri,
     state: state,
+    show_dialog: 'true'
   });
 
   return (
@@ -89,6 +90,6 @@ export const token = (): unknown => {
 };
 
 export const logout = (): unknown => {
-  accessToken = undefined;
+  accessToken = {};
   return;
 }
