da981a2b103bcd44679bf7db2117a6848e4eb12e
Nick Forleo <nickm140mc@gmail.com>
2024-11-08 14:19:02 -0500

    implement UT-22

--- server/__tests__/database.test.ts
+++ server/__tests__/database.test.ts
@@ -26,4 +26,36 @@ describe("Test the database functionality for the time statistic", () => {
         // Expect the time stat to be the initial stat
         expect(results.time).toBe(10000);
     });
+
+    test("A better score should be updated", async () => {
+        const statsWorst: UserStats = {
+            score: -1,
+            mistakes: 999999,
+            time: 10000
+        };
+
+        const statsBetter: UserStats = {
+            score: -1,
+            mistakes: 999999,
+            time: 5000
+        };
+
+        // Save the initial Time Stat
+        await databaseService.updateStats({
+            email: TEST_EMAIL,
+            stats: statsWorst
+        });
+
+        // Save the second game of stats with a better score
+        await databaseService.updateStats({
+            email: TEST_EMAIL,
+            stats: statsBetter
+        });
+
+        // Fetch the current time stat from the database
+        const results = await databaseService.getUserStats(TEST_EMAIL);
+
+        // Expect the time stat to be the the lower time
+        expect(results.time).toBe(5000);
+    });
 });
